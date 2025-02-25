85c3f2df4dc4743302f289fe95de290cbc28c8bb
Alejita18 <valerygame@hotmail.com>
2025-02-24 21:53:14 -0500

    Pruebas positivas actualizadas

--- configuration.py
+++ configuration.py
@@ -1,7 +1,3 @@
-URL_SERVICE = "https://cnt-2103bf60-92a5-44a7-a61d-6a022c6aaf91.containerhub.tripleten-services.com/"
-DOC_PATH = "/docs/"
-LOG_MAIN_PATH = "/api/logs/main/"
-USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+URL_SERVICE = "https://cnt-89851c50-4435-4723-ae45-5ad0076c9c40.containerhub.tripleten-services.com"
 CREATE_USER_PATH = "/api/v1/users/"
-PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
 CREATE_KITS = "/api/v1/kits"
--- sender_stand_request.py
+++ sender_stand_request.py
@@ -8,20 +8,13 @@ def post_new_user(body):
                          json=body,  # inserta el cuerpo de solicitud
                          headers=data.headers)  # inserta los encabezados
 
-response = post_new_user(data.user_body)
-print(response.status_code)
-print(response.json()['authToken'])
 
-authToken = response.json()['authToken']
-headers2 = {
-    "Content-Type": "application/json",
-    "Authorization": 'bearer {authToken}'
-}
 
-def post_new_client_kit(kit_body,authToken):
+def post_new_client_kit(kit_body):
     current_headers = data.headers.copy()  #referenciar y copiar los headers
-    current_headers["Authorization"] = "Bearer " + authToken #llamar el current y agrega la nueva llave authtoken
+    response = post_new_user(data.user_body)
+    token = response.json()['authToken']
+    current_headers["Authorization"] = f"Bearer {token}" #llamar el current y agrega la nueva llave authtoken
     return requests.post(configuration.URL_SERVICE + configuration.CREATE_KITS,
-                        json=kit_body,
+                         json=kit_body,
                          headers=current_headers)
-
