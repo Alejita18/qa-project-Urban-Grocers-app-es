0c3157e3d208b927e622737526202e71922d9f03
Alejita18 <valerygame@hotmail.com>
2025-02-24 22:17:21 -0500

    Correccion

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-89851c50-4435-4723-ae45-5ad0076c9c40.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-bbe29e33-4235-4257-9035-1370c1db536f.containerhub.tripleten-services.com"
 CREATE_USER_PATH = "/api/v1/users/"
 CREATE_KITS = "/api/v1/kits"
